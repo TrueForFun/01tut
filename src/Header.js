2af73bd40dab7d46944d89d3b728eb9b2527cb7d
True.for.fun <true.for.fun@gmail.com>
2024-05-16 08:16:54 +0300

    searchbox_added

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const Header = () => {
+export const Header = ({title}) => {
   
   return (
     <header>
-      <h1>Training List</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
